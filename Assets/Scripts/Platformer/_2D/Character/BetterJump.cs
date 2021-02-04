905c067c341cc5d4b4b535a087c5ace303546f66
Francis Paul Pasive <pasivefrancis@gmail.com>
2021-02-04 09:41:48 +0800

    Added BetterJump script

---
+++ Assets/Scripts/Platformer/_2D/Character/BetterJump.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+
+namespace FrancisPasive.Platformer._2D.Character
+{
+    public class BetterJump : MonoBehaviour
+    {
+        [Range(0f, 3f)] public float m_fallMultiplier = 0f;
+
+        [SerializeField] protected Rigidbody2D m_rigidbody;
+
+        private void Awake()
+        {
+            if (m_rigidbody == null)
+            {
+                m_rigidbody = GetComponent<Rigidbody2D>();
+                if (m_rigidbody == null)
+                {
+                    Debug.LogError("[Error] Missing rigidbody reference", this);
+                }
+            }
+        }
+
+        private void FixedUpdate()
+        {
+            // If falling
+            if (m_rigidbody.velocity.y < 0f)
+            {
+                float fallMultiplier = Mathf.Clamp(m_fallMultiplier - 1f, 0f, float.MaxValue);
+                m_rigidbody.velocity += Vector2.up * Physics2D.gravity.y * fallMultiplier * Time.fixedDeltaTime;
+            }
+        }
+
+    } // BetterJump end
+
+} // namespace end
+
